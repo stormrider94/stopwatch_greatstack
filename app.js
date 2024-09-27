4adcf08ba9fa89191278c93ba7c45fdf24da9ff9
stormrider94 <stormzephyr53@gmail.com>
2024-09-27 08:16:46 +0000

    stopwatch

---
+++ app.js
@@ -0,0 +1,44 @@
+let [seconds,minutes,hours] = [0,0,0]
+let displayTime = document.getElementById("displayTime")
+const startBtn = document.querySelector('img[src="images/start.png"]')
+let timer = null;
+
+function stopwatch(){
+    seconds++
+    if(seconds == 60){
+        seconds = 0
+        minutes++
+        if(minutes==60){
+            minutes = 0;
+            hours++
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours: hours
+    let m = minutes < 10 ? "0" + minutes : minutes
+    let s = seconds < 10 ? "0" + seconds : seconds
+    displayTime.innerHTML = h + ":" + m + ":" + s;
+}
+
+function watchStart(){
+    if(timer!== null){
+        clearInterval(timer)
+    }
+    timer = setInterval(stopwatch,1000)
+    
+}
+
+function watchStop(){
+    clearInterval(timer)
+}
+
+function watchReset(){
+    clearInterval(timer)
+
+    // instead of this line used in the original code [seconds, minutes, seconds] = [0,0,0]
+    // use this
+    seconds = 0
+    minutes = 0
+    hours = 0
+    displayTime.innerHTML = "00:00:00"
+}
